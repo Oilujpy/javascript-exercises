6a89387c4fae31f67fdb83102a34e39f3eb4e88c
Oilujpy <82006857+Oilujpy@users.noreply.github.com>
2025-04-24 18:44:40 -0400

    feat: solve exercise 05_sumAll
    ** Used Math.min() to find the smallest number between the two arguments.
    ** Used Math.max() to find the largest number between the two arguments.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function (min, max) {
+    let sum = 0;
+    let index = Math.min(min, max);
+    const range = Math.max(min, max);
 
+    if (min < 0 || max < 0
+        || Number.isInteger(min) === false
+        || Number.isInteger(max) === false) {
+        return "ERROR"
+    } else {
+        for (index; index <= range; index++) {
+            sum += index;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
