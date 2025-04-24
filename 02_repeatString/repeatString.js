bea66d355cfed64e9a008c637899c3edbe0a0e13
Oilujpy <82006857+Oilujpy@users.noreply.github.com>
2025-04-23 22:29:51 -0400

    feat: solve exercise 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+    let i = 0;
+    let result = "";
+    if (num >= 0) {
+        while (i < num) {
+            result += string;
+            i++;
+        }
+        return result;
+    } else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
