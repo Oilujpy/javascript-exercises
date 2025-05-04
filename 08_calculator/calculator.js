5a6abfbe1a3fe3512a4a74a910e52a1f3863d406
Oilujpy <82006857+Oilujpy@users.noreply.github.com>
2025-05-04 13:45:51 -0400

    feat: solve 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,39 @@
-const add = function() {
-	
+const add = function(a,b) {
+  const result = a + b;
+  return result;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	const result = a - b;
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  const result = array.reduce((sum, item) => sum + item, 0);
+  return +result;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  const result = array.reduce((operation, item) => operation * item, 1)
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  const result = a**b;
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let result = 1;
+  while (num > 0) {
+    result *= num;
+    num--;
+  }
+  return result;
 };
+
+console.log(factorial(8));
+
 
 // Do not edit below this line
 module.exports = {
