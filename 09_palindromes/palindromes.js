24340aac68f8e2dfcc93bc3eb649051c7c6585f9
Oilujpy <82006857+Oilujpy@users.noreply.github.com>
2025-05-06 14:46:04 -0400

    feat: solve 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+    const cleanInput = input.toLowerCase().replace(/[^0-9a-zA-Z]/g, "").split("");
+    const originalSplit = cleanInput.slice();
+    const reverseCleanInput = cleanInput.reverse();
+    for (let i = 0; i < cleanInput.length; i++) {
+        if (originalSplit[i] === reverseCleanInput[i]) {
+            continue; 
+        } else{
+           return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
