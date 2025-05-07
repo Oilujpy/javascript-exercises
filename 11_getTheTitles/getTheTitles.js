d68f32e952b52599b9b5d8da2b0f91cbfe7b27fa
Oilujpy <82006857+Oilujpy@users.noreply.github.com>
2025-05-07 17:56:52 -0400

    feat: solve 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arrayOfBooks) {
+    let titles = arrayOfBooks.map((item) => item.title);
+    return titles;
 };
 
 // Do not edit below this line
