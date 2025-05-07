e9434d1d6abe01980bb6ce004a0b61a7ea4fb536
Oilujpy <82006857+Oilujpy@users.noreply.github.com>
2025-05-07 17:51:47 -0400

    feat: solve 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+// for any n > 2: Fn = Fn-1 + Fn-2  
+const fibonacci = function (input) {
+    let cleanInput = Number(input);
+    if (cleanInput < 0) return "OOPS";
+    if (cleanInput === 0) return 0;
+    if (cleanInput === 1 || cleanInput === 2) return 1;
 
+    let result = 0;
+    const nMinus2 = fibonacci(input - 2);
+    const nMinus1 = fibonacci(input - 1);
+    result += nMinus1 + nMinus2;
+    return result;
 };
 
 // Do not edit below this line
