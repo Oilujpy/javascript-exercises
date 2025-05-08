14d2dcf90163e1a7258dc1267d9ac4fa1550fec7
Oilujpy <82006857+Oilujpy@users.noreply.github.com>
2025-05-08 14:40:48 -0400

    feat: solve 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arrayOfObjects) {
+    let oldest = arrayOfObjects.sort((a,b) => {
+        const CURRENT_YEAR = new Date().getFullYear();
+        if (!b.hasOwnProperty('yearOfDeath') || !a.hasOwnProperty('yearOfDeath')) {
+            return (CURRENT_YEAR - b.yearOfBirth) - (CURRENT_YEAR - a.yearOfBirth)
+        } else {
+            return (b.yearOfDeath - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth)
+        }
+    })
+    return oldest[0];
 };
 
 // Do not edit below this line
